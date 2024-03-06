4cb7bc667fd5889976dbcef1b38b34e6fa774fe4
trisston <trisstonprogramming@gmail.com>
2024-03-05 22:40:40 -0600

    Visual Updates and Temperature Toggle

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const content = document.getElementById("content");
 const form = document.getElementById("lookupForm");
-const cityInput = document.getElementById("city")
+const cityInput = document.getElementById("city");
+const tempTgl = document.getElementById("tempToggle");
+const cityName = document.getElementById("cityName");
 
 window.onload = () => {
     getWeather("http://api.weatherapi.com/v1/forecast.json?key=afc8a513ef9547378a1185517242802&q=New York&aqi=no&days=3") ;
@@ -8,25 +10,58 @@ window.onload = () => {
 
 form.addEventListener('submit', (element) =>{
     element.preventDefault();
-    clearContent();
-    getWeather(`http://api.weatherapi.com/v1/forecast.json?key=afc8a513ef9547378a1185517242802&q=${cityInput.value}&aqi=no&days=3`);
+    if(validate()){
+        clearContent();
+        getWeather(`http://api.weatherapi.com/v1/forecast.json?key=afc8a513ef9547378a1185517242802&q=${cityInput.value}&aqi=no&days=3`);
+    }
+    else{
+        alert("Please enter a valid location");
+    }
+    
+});
+
+tempTgl.addEventListener("click", () => {
+    if(validate()){
+        clearContent();
+        getWeather(`http://api.weatherapi.com/v1/forecast.json?key=afc8a513ef9547378a1185517242802&q=${cityInput.value}&aqi=no&days=3`);
+    }
 });
 
 async function getWeather(url){
+    const unit = getTempUnit();
     const response = await fetch(url,{mode: "cors"});
     response.json().then(function(response){
-        response.forecast.forecastday.forEach((element) => {
-            displayForecast(element)
-        });
+        try{
+            response.forecast.forecastday.forEach((element) => {
+                changeCityName(response.location.name);
+                displayForecast(element, unit);
+            });
+        }
+        catch(e){
+            alert("Please enter a valid location")
+        }
     });
 }
 
-function displayForecast(dayForecast){
+function displayForecast(dayForecast, tempUnit){
+    let tempHi;
+    let tempLo;
+    let unit;
+    if(tempUnit){
+        tempHi = dayForecast.day.maxtemp_c;
+        tempLo = dayForecast.day.mintemp_c;
+        unit = "C";
+    }
+    else {
+        tempHi = dayForecast.day.maxtemp_f;
+        tempLo = dayForecast.day.mintemp_f;
+        unit = "F";
+    }
     const daycard = document.createElement("div");
     daycard.classList.add("card");
     daycard.innerHTML = `<img src ="${dayForecast.day.condition.icon}" alt="${dayForecast.day.condition.text}"/>
-    <h5>${getDayName(dayForecast.date, navigator.languages)}</h5>
-    <h5>${dayForecast.day.maxtemp_f} / ${dayForecast.day.mintemp_f}</h5>`
+    <h4>${getDayName(dayForecast.date, navigator.languages)}</h4>
+    <h4>${tempHi} / ${tempLo} ${unit}</h4>`
     content.appendChild(daycard);
 }
 
@@ -40,4 +75,21 @@ function getDayName(dateStr, locale)
 {
     var date = new Date(dateStr);
     return date.toDateString(locale, { weekday: 'long' });        
+}
+
+function getTempUnit(){
+    return tempTgl.checked;
+}
+
+function validate(){
+    if(cityInput.value == ""){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function changeCityName(name){
+    cityName.textContent = name;
 }
